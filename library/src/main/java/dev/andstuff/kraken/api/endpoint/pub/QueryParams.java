87c9cd5b38ba0b6d80a1310270fcd445f065c1e7
nyg <nyg@users.noreply.github.com>
2025-11-29 13:05:00 +0100

    fix: avoid sending query params for AssetPairs endpoint

--- library/src/main/java/dev/andstuff/kraken/api/endpoint/pub/QueryParams.java
+++ library/src/main/java/dev/andstuff/kraken/api/endpoint/pub/QueryParams.java
@@ -1,10 +1,17 @@
 package dev.andstuff.kraken.api.endpoint.pub;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public interface QueryParams {
 
     QueryParams EMPTY = Map::of;
 
     Map<String, String> toMap();
+
+    static <T> void putIfNonNull(Map<String, String> map, String key, T value, Function<T, String> apply) {
+        if (value != null) {
+            map.put(key, apply.apply(value));
+        }
+    }
 }
